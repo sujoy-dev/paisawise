e31e337736a80ccbf26c838469ef9ea951ae6dd2
sujoy-dev <banerjeesujoy007@gmail.com>
2024-05-15 19:54:32 +0530

    first commit

---
+++ client/src/scenes/navbar/index.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
+import { Box, Typography, useTheme } from "@mui/material";
+import FlexBetween from "@/components/FlexBetween";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  const { palette } = useTheme();
+  const [selected, setSelected] = useState("dashboard");
+  return (
+    <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
+      {/* Left Side */}
+      <FlexBetween gap="0.25rem">
+        <CurrencyRupeeIcon sx={{ fontSize: "28px" }} />
+        <Link to="/" style={{ textDecoration: "inherit" }}>
+          <Typography variant="h4" fontSize="16px">
+            PaisaWise
+          </Typography>
+        </Link>
+      </FlexBetween>
+
+      {/* Right Side */}
+      <FlexBetween gap="2rem">
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/"
+            onClick={() => setSelected("dashboard")}
+            style={{
+              color: selected === "dashboard" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            dashboard
+          </Link>
+        </Box>
+        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
+          <Link
+            to="/predictions"
+            onClick={() => setSelected("predictions")}
+            style={{
+              color: selected === "predictions" ? "inherit" : palette.grey[700],
+              textDecoration: "inherit",
+            }}
+          >
+            predictions
+          </Link>
+        </Box>
+      </FlexBetween>
+    </FlexBetween>
+  );
+};
+
+export default Navbar;
